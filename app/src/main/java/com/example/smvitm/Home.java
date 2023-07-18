46002ffbf47895d0f800824da16cb90605a1980d
ramac <ramachandraudupa2004@gmail.com>
2023-07-18 08:04:49 +0530

    messages updated

--- app/src/main/java/com/example/smvitm/Home.java
+++ app/src/main/java/com/example/smvitm/Home.java
@@ -3,6 +3,7 @@ package com.example.smvitm;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.Menu;
 import android.widget.TextView;
@@ -92,6 +93,7 @@ public class Home extends AppCompatActivity {
         getMenuInflater().inflate(R.menu.home, menu);
         return true;
     }
+
 
     @Override
     public boolean onSupportNavigateUp() {
