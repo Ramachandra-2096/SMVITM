1b297f4cd3641fa09a8e55cddc782537f7b44c2b
ramac <ramachandraudupa2004@gmail.com>
2023-08-15 06:58:20 +0530

    messages updated

--- app/src/main/java/com/example/smvitm/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/smvitm/ui/home/HomeFragment.java
@@ -1,32 +1,52 @@
 package com.example.smvitm.ui.home;
 
+import android.content.Intent;
+import android.graphics.ImageDecoder;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.smvitm.Home;
+import com.example.smvitm.MainActivity;
+import com.example.smvitm.R;
+import com.example.smvitm.ZoomedActivity;
 import com.example.smvitm.databinding.FragmentHomeBinding;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class HomeFragment extends Fragment {
 
     private FragmentHomeBinding binding;
+    private TextView noPostsText;
+    private Button addPostButton;
+    private RecyclerView postsRecyclerView;
+    private FirebaseFirestore firestore;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        HomeViewModel homeViewModel =
-                new ViewModelProvider(this).get(HomeViewModel.class);
+        View rootview = inflater.inflate(R.layout.fragment_home,container,false);
 
-        binding = FragmentHomeBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
+        noPostsText = rootview.findViewById(R.id.noPostsText);
+        postsRecyclerView = rootview.findViewById(R.id.postsRecyclerView);
+        firestore = FirebaseFirestore.getInstance();
 
-        final TextView textView = binding.textHome;
-        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
-        return root;
+        Button addButton = rootview.findViewById(R.id.addPostButton);
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), ZoomedActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        return rootview;
     }
 
     @Override
